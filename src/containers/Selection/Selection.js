c119b4260424c5e247a29fb36c6fe62d4199bb97
Sjoerd <s.weerstra@student.fontys>
2018-02-04 13:57:47 +0100

    Responsive design with 800px max-width media query

--- src/containers/Selection/Selection.js
+++ src/containers/Selection/Selection.js
@@ -157,9 +157,10 @@ class SelectionContainer extends Component {
                  stroke="white"
                  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                  className={`feather feather-folder-plus clickable ${librariesVisible ? 'active' : ''}`}
-                 onClick={this.showLibraries.bind(this)}
-                 title="Toggle Libraries">
-              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+                 onClick={this.showLibraries.bind(this)}>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z">
+                <title>Toggle Libraries</title>
+              </path>
               <line x1="12" y1="11" x2="12" y2="17"></line>
               <line x1="9" y1="14" x2="15" y2="14"></line>
             </svg>
